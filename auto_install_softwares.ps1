60faf7f04af3334a8ad36816054dadce7687ab40
zen <zeng_weizhi@foxmail.com>
2022-09-09 15:05:46 +0800

    更新readme，初始化软件安装脚本

---
+++ auto_install_softwares.ps1
@@ -0,0 +1,19 @@
+# install scoop
+irm get.scoop.sh | iex
+scoop bucket add java
+scoop bucket add extras
+
+# install shell tools
+# git | powershell 7 | starship | windows ternimal
+scoop install git
+scoop install powershell 
+scoop install starship
+
+# install dev languages
+# nodejs | python | go | java
+scoop install nodejs
+scoop install python
+scoop install go
+scoop install java
+
+# install dev tools
